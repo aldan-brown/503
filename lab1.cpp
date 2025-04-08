451618d30650ad66e1a3a0ae94bfb988cba4069b
Aldan Brown <aldanb@uw.edu>
2025-04-08 11:20:56 -0700

    Lab1 complete

--- lab1.cpp
+++ lab1.cpp
@@ -10,37 +10,36 @@ using namespace std;
 
 queue<int> pids; // stores a list of processes from the parent to a leaf process
 
-void recursive_creation(int leaves)
-
-{
-
+void recursive_creation(int leaves) {
    if (leaves > 1) {
       pids.push(getpid());
 
       // fork a left child
       int leftChild = fork();
+
       if (leftChild == 0) {
-         recursive_creation(leaves / 2);
+         recursive_creation(leaves - leaves / 2);
          exit(0);
       }
+
+      int status;
+      // wait for one of the children
+      wait(&status);
 
       // fork a right child
       int rightChild = fork();
       if (rightChild == 0) {
-         recursive_creation(leaves - (leaves / 2));
+         recursive_creation(leaves/2);
          exit(0);
       }
 
-      // wait for one of the children
-
       // wait for the other child
+      wait(&status);
 
       exit(0);
 
    } else { // I'm a leaf process
-      while (pids.size() > 0)
-
-      { // print out a list of my ascendants
+      while (pids.size() > 0) { // print out a list of my ascendants
          cout << pids.front() << " ";
          pids.pop();
       }
@@ -49,24 +48,20 @@ void recursive_creation(int leaves)
    }
 }
 
-int main(int argc, char* argv[])
-
-{
+int main(int argc, char* argv[]){
    // validate arguments
-   if (argc != 2)
-
-   {
+   if (argc != 2) {
       cerr << "usage: lab1 #leaves" << endl;
       return -1;
    }
-   int leaves = atoi(argv[1]);
-   if (leaves < 1)
 
-   {
+   int leaves = atoi(argv[1]);
+   if (leaves < 1) {
       cerr << "usage: lab1 #leaves" << endl;
       cerr << "where #leaves >= 1" << endl;
       return -1;
    }
+
    recursive_creation(leaves);
    return 0;
 }
