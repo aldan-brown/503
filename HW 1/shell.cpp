4f1e343253a078d85ce77ed9e0193723715dca43
Aldan Brown <aldanb@uw.edu>
2025-04-09 17:17:20 -0700

    Comment updates

--- HW 1/shell.cpp
+++ HW 1/shell.cpp
@@ -4,10 +4,10 @@
 // Date Modified: 4/9/2025
 // ------------------------------------------------------------------------------------------------
 // Description: This program implements a simple shell that can execute commands and handle
-// input/output redirection. It uses fork and exec to create child processes for executing commands.
-// The shell continues to run until the user enters "exit" or "quit". The shell also supports
-// background execution of commands using the '&' symbol, repeating commands using "!!", and 
-// connection of commands together using "<" and ">".
+// input/output redirection. It uses fork and exec to create child processes for executing 
+// commands. The shell continues to run until the user enters "exit" or "quit". The shell also 
+// supports background execution of commands using the '&' symbol, repeating commands using "!!", 
+// and connection of commands together using "<" and ">".
 // ------------------------------------------------------------------------------------------------
 
 #include <iostream>
