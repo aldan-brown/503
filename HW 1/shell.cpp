c73baa293ecf7df640f8d019ce02a4c25fe3ec0b
Aldan Brown <aldanb@uw.edu>
2025-04-10 13:43:22 -0700

    Added < and >

--- HW 1/shell.cpp
+++ HW 1/shell.cpp
@@ -4,12 +4,13 @@
 // Date Modified: 4/9/2025
 // ------------------------------------------------------------------------------------------------
 // Description: This program implements a simple shell that can execute commands and handle
-// input/output redirection. It uses fork and exec to create child processes for executing 
-// commands. The shell continues to run until the user enters "exit" or "quit". The shell also 
-// supports background execution of commands using the '&' symbol, repeating commands using "!!", 
+// input/output redirection. It uses fork and exec to create child processes for executing
+// commands. The shell continues to run until the user enters "exit" or "quit". The shell also
+// supports background execution of commands using the '&' symbol, repeating commands using "!!",
 // and connection of commands together using "<" and ">".
 // ------------------------------------------------------------------------------------------------
 
+#include <fcntl.h>
 #include <iostream>
 #include <queue>
 #include <sstream>
@@ -27,8 +28,8 @@ using namespace std;
 const int MAX_LINE = 80;
 
 int main() {
-   string input, history;
-   bool should_run = true;
+   string input, history;  // Command line inputs and a copy for history
+   bool should_run = true; // Exit command receipt
 
    while (should_run) {
       cout << "osh> " << flush; // Prompt for user
@@ -87,6 +88,33 @@ int main() {
          }
       }
 
+      // Handle file inputs (<) and outputs (>)
+      bool fileInput = false;
+      bool fileOutput = false;
+      string iFile, oFile;
+
+      for (int i = 0; i < tokens.size(); i++) {
+         if (tokens[i] == "<") {
+            fileInput = true;
+            if (tokens.size() > (i + 1)) { // Check file name exists
+               iFile = tokens[i + 1];
+
+               // Delete < and file name before parsing to args
+               tokens.erase(tokens.begin() + i, tokens.begin() + i + 2);
+            }
+            break;
+         } else if (tokens[i] == ">") {
+            fileOutput = true;
+            if (tokens.size() > (i + 1)) { // Check file name exists
+               oFile = tokens[i + 1];
+
+               // Delete > and file name before parsing to args
+               tokens.erase(tokens.begin() + i, tokens.begin() + i + 2);
+            }
+            break;
+         }
+      }
+
       // Convert vector<string> to char* array for execvp
       vector<char*> args;
       for (string& s : tokens) {
@@ -102,6 +130,26 @@ int main() {
          exit(1);
       } else if (pid == 0) {
          // Child process
+         if (fileInput) { // Check input
+            int fd = open(iFile.c_str(), O_RDONLY); // open file in read-only
+            if (fd < 0) { // Open error check
+               cerr << "Input file open failed: " << iFile << endl;
+               exit(1);
+            }
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+         }
+
+         if (fileOutput) {
+            int fd = open(oFile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0) {
+               cerr << "Output file open failed: "<< oFile << endl;
+               exit(1);
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+         }
+
          execvp(args[0], args.data()); // Execute command
          cerr << "Command not found: " << args[0] << endl;
          exit(1);
