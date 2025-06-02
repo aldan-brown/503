589d79db1d1e7999031b810bb5e9d58750c6fdbe
Aldan Brown <aldanb@uw.edu>
2025-06-02 14:21:48 -0700

    Program works! Script for testing created

---
+++ Program4/client.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+g++ Client.cpp -o client
+
+chmod 755 client
+
+# Define buffer combinations
+buf_combinations="15 100
+30 50
+60 25
+100 15"
+
+# Define test types
+test_types="1 2 3"
+
+# Clear output file
+> output.txt
+
+# Run combinations
+for combo in $buf_combinations
+do
+    set -- $combo
+    nbufs=$1
+    bufsize=$2
+
+    for type in $test_types
+    do
+        echo "Running: ./client csslab8.uwb.edu 04171 20000 $nbufs $bufsize $type" >> output.txt
+        ./client csslab8.uwb.edu 04171 20000 $nbufs $bufsize $type | grep "Test(" >> output.txt
+    done
+done
